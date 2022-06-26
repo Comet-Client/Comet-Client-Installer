7d9b78d020023df769caa75db1a6cf785d77dc08
PetrTech <55279432+PetrTech@users.noreply.github.com>
2022-06-26 10:13:09 +0200

    Add project files.

---
+++ Comet Client Launcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comet_Client_Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
